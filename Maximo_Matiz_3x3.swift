4c57894b9c3b9d958b5645ef01ff92f71bda9143
Carlol3097 <128756752+Carlol3097@users.noreply.github.com>
2026-02-15 20:59:11 -0700

    Update Maximo_Matiz_3x3.swift

--- Maximo_Matiz_3x3.swift
+++ Maximo_Matiz_3x3.swift
@@ -1,45 +1,265 @@
 import Foundation
 
-print("MATRIZ 3x3 - ENCONTRAR VALOR MÁXIMO")
-print("-----------------------------------")
+print(" Valor Máximo Matriz 3x3 \n")
 
 var matriz: [[Int]] = Array(repeating: Array(repeating: 0, count: 3), count: 3)
+var numeroMaximo: Int = 0
+var i: Int = 0
+var j: Int = 0
 
-// Llenar matriz
-for i in 0..<3 {
-    for j in 0..<3 {
-        print("Ingrese el número en la posición [\(i)][\(j)]: ", terminator: "")
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" Ingrese el número en la posición [\(x+1)][\(y+1)]: ", terminator: "")
         if let input = readLine(), let numero = Int(input) {
-            matriz[i][j] = numero
+            matriz[x][y] = numero
         }
     }
 }
 
-// Mostrar matriz
-print("\nMatriz ingresada:")
-for i in 0..<3 {
-    for j in 0..<3 {
-        print(" \(matriz[i][j]) ", terminator: "")
+print("\n")
+print(" Matriz")
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" \(matriz[x][y]) ", terminator: "")
     }
     print()
 }
 
-// Encontrar valor máximo y su posición
-var maximo = matriz[0][0]
-var posicionI = 0
-var posicionJ = 0
+numeroMaximo = matriz[0][0]
+i = 0
+j = 0
 
-for i in 0..<3 {
-    for j in 0..<3 {
-        if matriz[i][j] > maximo {
-            maximo = matriz[i][j]
-            posicionI = i
-            posicionJ = j
+for x in 0..<3 {
+    for y in 0..<3 {
+        if matriz[x][y] > numeroMaximo {
+            numeroMaximo = matriz[x][y]
+            i = x
+            j = y
         }
     }
 }
 
-print("\n--- RESULTADOS ---")
-print("El valor máximo es: \(maximo)")
-print("Se encuentra en la posición: [\(posicionI)][\(posicionJ)]")
-print("Fila: \(posicionI + 1), Columna: \(posicionJ + 1)")
+print("\n")
+print(" El valor máximo es: \(numeroMaximo)")
+print(" Se encuentra en la posición: [\(i)][\(j)]")
+print(" Fila: \(i + 1), Columna: \(j + 1)")import Foundation
+
+print(" Valor Máximo Matriz 3x3 \n")
+
+var matriz: [[Int]] = Array(repeating: Array(repeating: 0, count: 3), count: 3)
+var numeroMaximo: Int = 0
+var i: Int = 0
+var j: Int = 0
+
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" Ingrese el número en la posición [\(x+1)][\(y+1)]: ", terminator: "")
+        if let input = readLine(), let numero = Int(input) {
+            matriz[x][y] = numero
+        }
+    }
+}
+
+print("\n")
+print(" Matriz")
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" \(matriz[x][y]) ", terminator: "")
+    }
+    print()
+}
+
+numeroMaximo = matriz[0][0]
+i = 0
+j = 0
+
+for x in 0..<3 {
+    for y in 0..<3 {
+        if matriz[x][y] > numeroMaximo {
+            numeroMaximo = matriz[x][y]
+            i = x
+            j = y
+        }
+    }
+}
+
+print("\n")
+print(" El valor máximo es: \(numeroMaximo)")
+print(" Se encuentra en la posición: [\(i)][\(j)]")
+print(" Fila: \(i + 1), Columna: \(j + 1)")import Foundation
+
+print(" Valor Máximo Matriz 3x3 \n")
+
+var matriz: [[Int]] = Array(repeating: Array(repeating: 0, count: 3), count: 3)
+var numeroMaximo: Int = 0
+var i: Int = 0
+var j: Int = 0
+
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" Ingrese el número en la posición [\(x+1)][\(y+1)]: ", terminator: "")
+        if let input = readLine(), let numero = Int(input) {
+            matriz[x][y] = numero
+        }
+    }
+}
+
+print("\n")
+print(" Matriz")
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" \(matriz[x][y]) ", terminator: "")
+    }
+    print()
+}
+
+numeroMaximo = matriz[0][0]
+i = 0
+j = 0
+
+for x in 0..<3 {
+    for y in 0..<3 {
+        if matriz[x][y] > numeroMaximo {
+            numeroMaximo = matriz[x][y]
+            i = x
+            j = y
+        }
+    }
+}
+
+print("\n")
+print(" El valor máximo es: \(numeroMaximo)")
+print(" Se encuentra en la posición: [\(i)][\(j)]")
+print(" Fila: \(i + 1), Columna: \(j + 1)")import Foundation
+
+print(" Valor Máximo Matriz 3x3 \n")
+
+var matriz: [[Int]] = Array(repeating: Array(repeating: 0, count: 3), count: 3)
+var numeroMaximo: Int = 0
+var i: Int = 0
+var j: Int = 0
+
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" Ingrese el número en la posición [\(x+1)][\(y+1)]: ", terminator: "")
+        if let input = readLine(), let numero = Int(input) {
+            matriz[x][y] = numero
+        }
+    }
+}
+
+print("\n")
+print(" Matriz")
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" \(matriz[x][y]) ", terminator: "")
+    }
+    print()
+}
+
+numeroMaximo = matriz[0][0]
+i = 0
+j = 0
+
+for x in 0..<3 {
+    for y in 0..<3 {
+        if matriz[x][y] > numeroMaximo {
+            numeroMaximo = matriz[x][y]
+            i = x
+            j = y
+        }
+    }
+}
+
+print("\n")
+print(" El valor máximo es: \(numeroMaximo)")
+print(" Se encuentra en la posición: [\(i)][\(j)]")
+print(" Fila: \(i + 1), Columna: \(j + 1)")import Foundation
+
+print(" Valor Máximo Matriz 3x3 \n")
+
+var matriz: [[Int]] = Array(repeating: Array(repeating: 0, count: 3), count: 3)
+var numeroMaximo: Int = 0
+var i: Int = 0
+var j: Int = 0
+
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" Ingrese el número en la posición [\(x+1)][\(y+1)]: ", terminator: "")
+        if let input = readLine(), let numero = Int(input) {
+            matriz[x][y] = numero
+        }
+    }
+}
+
+print("\n")
+print(" Matriz")
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" \(matriz[x][y]) ", terminator: "")
+    }
+    print()
+}
+
+numeroMaximo = matriz[0][0]
+i = 0
+j = 0
+
+for x in 0..<3 {
+    for y in 0..<3 {
+        if matriz[x][y] > numeroMaximo {
+            numeroMaximo = matriz[x][y]
+            i = x
+            j = y
+        }
+    }
+}
+
+print("\n")
+print(" El valor máximo es: \(numeroMaximo)")
+print(" Se encuentra en la posición: [\(i)][\(j)]")
+print(" Fila: \(i + 1), Columna: \(j + 1)")import Foundation
+
+print(" Valor Máximo Matriz 3x3 \n")
+
+var matriz: [[Int]] = Array(repeating: Array(repeating: 0, count: 3), count: 3)
+var numeroMaximo: Int = 0
+var i: Int = 0
+var j: Int = 0
+
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" Ingrese el número en la posición [\(x+1)][\(y+1)]: ", terminator: "")
+        if let input = readLine(), let numero = Int(input) {
+            matriz[x][y] = numero
+        }
+    }
+}
+
+print("\n")
+print(" Matriz")
+for x in 0..<3 {
+    for y in 0..<3 {
+        print(" \(matriz[x][y]) ", terminator: "")
+    }
+    print()
+}
+
+numeroMaximo = matriz[0][0]
+i = 0
+j = 0
+
+for x in 0..<3 {
+    for y in 0..<3 {
+        if matriz[x][y] > numeroMaximo {
+            numeroMaximo = matriz[x][y]
+            i = x
+            j = y
+        }
+    }
+}
+
+print("\n")
+print(" El valor máximo es: \(numeroMaximo)")
+print(" Se encuentra en la posición: [\(i)][\(j)]")
+print(" Fila: \(i + 1), Columna: \(j + 1)")
